384bf2ad4c517d78ba7f04dc375bc776fd08e5f6
Rory Murray <rory.christian.murray@gmail.com>
2019-06-19 19:10:41 -0400

    Render WeatherViewContainer

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import WeatherViewContainer from "./views/WeatherViewContainer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Rory's Weather App</h1>
+      <WeatherViewContainer />
     </div>
   );
 }
