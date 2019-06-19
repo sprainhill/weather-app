a4ec0c8f358ca7ea518fe7ec88ecb55295cb06fa
Rory Murray <rory.christian.murray@gmail.com>
2019-06-19 17:56:42 -0400

    project scaffold

---
+++ weather/src/views/WeatherViewContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 
+
+const WeatherViewContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WeatherViewContainer;
